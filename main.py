0b9c21aae195d5f5e7065234a2776e710c2d9315
DaniG33 <danigtech18@gmail.com>
2025-11-13 16:37:01 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+numero1 = int
+numero2 = int
+
+numero1 = int (input("Ingresa el primer número: "))
+numero2 = int (input("Ingresa el segundo número: "))
+
+resultado = numero1 + numero2
+
+print("La suma es:", resultado)
